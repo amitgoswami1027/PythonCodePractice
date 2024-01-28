f428383a3e758fb92066c55e514e4e0a3f570f48
Amit Goswami <amitgoswami1027@gmail.com>
2024-01-29 05:24:05 +0530

    flight club

--- CapStone_FlightDeals/main.py
+++ CapStone_FlightDeals/main.py
@@ -4,36 +4,54 @@ from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
 
-#APIs Required
-#Google Sheet Data Management - https://sheety.co/
-#Kiwi Partners Flight Search API (Free Signup, Credit Card not required) - https://partners.kiwi.com/
-#Tequila Flight Search API Documentation - https://tequila.kiwi.com/portal/docs/tequila_api
-#Twilio SMS API - https://www.twilio.com/docs/sms
+ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
-ORIGIN_CITY_IATA = "LON"
+sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)
     data_manager.update_destination_codes()
+    sheet_data = data_manager.get_destination_data()
+
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
 
 tomorrow = datetime.now() + timedelta(days=1)
-six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
 
-for destination in sheet_data:
+for destination_code in destinations:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
-        destination["iataCode"],
+        destination_code,
         from_time=tomorrow,
         to_time=six_month_from_today
     )
-    if flight.price < destination["lowestPrice"]:
-        notification_manager.send_sms(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-        )
+    print(flight.price)
+    if flight is None:
+        continue
+
+    if flight.price < destinations[destination_code]["price"]:
+
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
+
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+
+       
+        
+        notification_manager.send_emails(emails, message)
+
+
